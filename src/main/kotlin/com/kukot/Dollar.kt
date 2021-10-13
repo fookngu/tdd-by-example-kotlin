49f0e4b127ba72f89416029d55d10e3fd611cc9f
kukot <phucnn.work@gmail.com>
2021-10-14 01:25:31 +0700

    - equality, privacy

--- src/main/kotlin/com/kukot/Dollar.kt
+++ src/main/kotlin/com/kukot/Dollar.kt
@@ -1,9 +1,13 @@
 package com.kukot
 
-class Dollar(val amount: Int) {
+class Dollar(private var amount: Int) {
 
     fun times(multiplier: Int): Dollar {
         val newAmount = multiplier * amount
         return Dollar(newAmount)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other is Dollar && other.amount == amount
     }
 }
--- src/test/kotlin/com/kukot/MoneyTest.kt
+++ src/test/kotlin/com/kukot/MoneyTest.kt
@@ -2,21 +2,28 @@ package com.kukot
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class MoneyTest {
 
     // TODO:
     // $5 + 10 CHF = $10 (with rate of 2:1)
-    // $5 * 2 = $10
-    // make amount private
-    // dollar side effects
+    // $5 * 2 = $10 - DONE
+    // make amount private - DONE
+    // dollar side effects - DONE
     // money rounding
     @Test
     fun testMultiplication() {
         val fiveDollar = Dollar(5)
         var newDollar: Dollar = fiveDollar.times(2)
-        assertEquals(10, newDollar.amount)
+        assertEquals(Dollar(10), newDollar)
         newDollar = fiveDollar.times(3)
-        assertEquals(15, newDollar.amount)
+        assertEquals(Dollar(15), newDollar)
+    }
+
+    @Test
+    fun testEquality() {
+        assertEquals(Dollar(7), Dollar(7))
+        assertNotEquals(Dollar(10), Dollar(20))
     }
 }
